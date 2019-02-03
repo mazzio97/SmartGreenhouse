83f25c836360c99576346fbed93b7a99a1bbb020
Diego Mazzieri <diego.maz97@gmail.com>
2019-02-03 15:39:10 +0100

    Client for socket connection example

---
+++ src/org/sgh/client/SocketClientExample.java
@@ -0,0 +1,40 @@
+package org.sgh.client;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class SocketClientExample {
+
+    public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
+        //get the localhost IP address, if server is running on some other IP, you need to use that
+        InetAddress host = InetAddress.getLocalHost();
+        Socket socket = null;
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+        Scanner userInput = new Scanner(System.in);
+        String msg = "";
+        while (msg != "exit") {
+            //establish socket connection to server
+            socket = new Socket(host.getHostName(), 9876);
+            //write to socket using ObjectOutputStream
+            oos = new ObjectOutputStream(socket.getOutputStream());
+            System.out.print("Write message to send: ");
+            msg = userInput.next();
+            oos.writeObject(msg);
+            //read the server response message
+            ois = new ObjectInputStream(socket.getInputStream());
+            String message = (String) ois.readObject();
+            System.out.println("Message: " + message);
+            //close resources
+            ois.close();
+            oos.close();
+            Thread.sleep(1000);
+        }
+        userInput.close();
+    }
+}

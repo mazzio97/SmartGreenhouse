ac9bd849606ca4797d7715c43e901f0ab3dae5f1
Diego Mazzieri <diego.maz97@gmail.com>
2019-02-03 17:57:00 +0100

    Receive data from the server in client socket

--- sgh-client/src/org/sgh/client/SocketClientExample.java
+++ sgh-client/src/org/sgh/client/SocketClientExample.java
@@ -1,40 +1,22 @@
 package org.sgh.client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.InetAddress;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 public class SocketClientExample {
 
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
-        //get the localhost IP address, if server is running on some other IP, you need to use that
-        InetAddress host = InetAddress.getLocalHost();
-        Socket socket = null;
-        ObjectOutputStream oos = null;
-        ObjectInputStream ois = null;
-        Scanner userInput = new Scanner(System.in);
+        Socket socket;
         String msg = "";
-        while (msg != "exit") {
-            //establish socket connection to server
-            socket = new Socket(host.getHostName(), 9876);
-            //write to socket using ObjectOutputStream
-            oos = new ObjectOutputStream(socket.getOutputStream());
-            System.out.print("Write message to send: ");
-            msg = userInput.next();
-            oos.writeObject(msg);
-            //read the server response message
-            ois = new ObjectInputStream(socket.getInputStream());
-            String message = (String) ois.readObject();
-            System.out.println("Message: " + message);
-            //close resources
-            ois.close();
-            oos.close();
-            Thread.sleep(1000);
+        while (!msg.equals("exit")) {
+            socket = new Socket("192.168.1.115", 9875);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            msg = in.readLine();
+            System.out.println(msg);
         }
-        userInput.close();
+        System.out.println("I'm going to die...");
     }
 }
